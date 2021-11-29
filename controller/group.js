a08c55cee9ce183bb558d3a2aed0a7d4940327d5
lavgin7 <oponeuser7@gmail.com>
2021-11-29 23:07:21 +0900

    fix: get left side boxes fixed

---
+++ controller/group.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#members-header").css("color", "gray");
+    $("#members-header").css("cursor", "pointer");
+        $("#add-schedule-header").css("cursor", "pointer");
+
+    $("#members-header").click(function(){
+        $("#add-schedule-header").css("color", "gray");
+        $("#members-header").css("color", "black");
+        $(".add-schedule-box").hide();
+        $("#members-box").show();
+    });
+    
+    $("#add-schedule-header").click(function() {
+        $("#members-header").css("color", "gray");
+        $("#add-schedule-header").css("color", "black");
+        $("#members-box").hide();
+        $(".add-schedule-box").show();
+    })
+});
--- controller/home.js
+++ controller/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     //로그인 버튼을 생성
     $("#home-buttons").append($("<div>Sign in</div>").attr("id", "sign-in"));
-	
+
 	$("#sign-in").click(function(event) {
         $("#sign-in-window").toggle();
     });
