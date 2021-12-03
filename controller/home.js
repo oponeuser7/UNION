aa61a73fdcfe72f71bb49811e8760898c038ff5d
lavgin7 <oponeuser7@gmail.com>
2021-12-03 23:37:14 +0900

    feat: add create group button - window

--- controller/home.js
+++ controller/home.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-    //로그인 버튼을 생성
-    $("#home-buttons").append($("<div>Sign in</div>").attr("id", "sign-in"));
+  //로그인 버튼을 생성
+  $("#home-buttons").append($("<div>Sign in</div>").attr("id", "sign-in"));
 
 	$("#sign-in").click(function(event) {
-        $("#sign-in-window").toggle();
-    });
+    $("#sign-in-window").toggle();
+  });
+
+  $("#create-group").click(function(event) {
+    $(event.target).hide();
+    $("#create-group-input-row").show();
+  }); 
+  
+  $("#create-group-input-button").click(function(event) {
+    $("#create-group-input-row").hide();
+    $("#create-group").show();
+  });
+
 });
