c5132831e5653e000bebc001034e9947a9fce1b2
lavgin7 <oponeuser7@gmail.com>
2021-12-07 00:23:48 +0900

    feat: sign up

---
+++ model/signUp.php
@@ -0,0 +1,16 @@
+<?php
+$user = new stdClass();
+$user->id = $_POST["id"];
+$user->pwd = $_POST["pwd"];
+$file = fopen("data/user.json", "r");
+$content = [];
+while(!feof($file)) {
+	$temp = trim(fgets($file));
+	if($temp) $content[] = $temp;
+}
+fclose($file);
+$content[] = json_encode($user);
+$file = fopen("data/user.json", "w");
+fwrite($file, implode("\n", $content));
+fclose($file);
+?>
